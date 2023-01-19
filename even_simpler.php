da973724645635e61f26b7a26eb0bb9d39c2fb15
O. Inha <50331907+olaviinha@users.noreply.github.com>
2023-01-20 01:47:17 +0200

    Even simpler example

---
+++ even_simpler.php
@@ -0,0 +1,15 @@
+<?php
+  $token = ''; // Telegram bot token
+  $path = "https://api.telegram.org/bot".$token;
+  $update = json_decode(file_get_contents("php://input"), TRUE);
+
+  if(array_key_exists('message', $update)){
+    $chat_id = $update["message"]["chat"]["id"];
+    if(array_key_exists('text', $update["message"])) $message = strip_tags($update["message"]["text"]);
+  }
+
+  if (strpos($message, "/somecommand") === 0) {
+    // Do a thing
+    file_get_contents($path."/sendmessage?chat_id=".$chat_id."&text=".$answer."&parse_mode=html");
+  }
+?>
