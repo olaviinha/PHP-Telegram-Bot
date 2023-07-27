fbeacb76e285e53043d50db40e19f5c27222c8be
O. Inha <50331907+olaviinha@users.noreply.github.com>
2023-07-27 17:36:40 +0300

    Create locale.php

---
+++ locale.php
@@ -0,0 +1,101 @@
+<?php 
+
+  $lang = "fi";
+
+  $msgs['en'] = array(
+    'error' => array(
+      "Sorry, I was unable to execute that.",
+      "Sorry, something went wrong just now.",
+      "Hmm that did not compute."
+    ),
+    'not_allowed' => "Bot is not allowed to operate in this chat.",
+    'chat_allowed' => "Bot can now operate in this chat.",
+    'chat_disallowed' => "Bot can no longer operate in this chat",
+    'already_allowed' => "Bot can already operate in this chat.",
+    'already_disallowed' => "Bot operations are already disabled in this chat.",
+    'message_sent' => "Message sent.",
+    'command_disabled' => "Command temporarily disabled.",
+    'not_understood' => array(
+      "I'm not sure what you mean.",
+      "Could you be more specific?",
+      "Uhh what?",
+      "lol wut"
+    ),
+    'ok' => array(
+      "ok",
+      "done",
+      "👍"
+    ),
+    'ok_remove' => array(
+      "Got it",
+      "Gotcha",
+      "Alright",
+      "Done",
+      "Remove this",
+    ),
+    'weekdays' => array(
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday'
+    ),
+    'at_weekday' => '',
+  );
+
+  $msgs['fi'] = array(
+    'error' => array(
+      "Homma kusi nyt jotenki...",
+      "Hmm, ei natsannu.",
+      "Öö meni vituiks.."
+    ),
+    'not_allowed' => "Botti ei toimi tässä chatissä.",
+    'chat_allowed' => "Botti toimii nyt tässä chatissä.",
+    'chat_disallowed' => "Botti ei enää toimi tässä chatissä.",
+    'already_allowed' => "Botti toimii jo tässä chatissä.",
+    'already_disallowed' => "Botti on jo disabloitu tääl...",
+    'message_sent' => "Viesti lähetetty.",
+    'command_disabled' => "Komento ei ole tällä hetkellä käytössä.",
+    'not_understood' => array(
+      "En ymmärrä.",
+      "Voisitko tarkentaa?",
+      "lol wut"
+    ),
+    'ok' => array(
+      "Ok 👍",
+      "Juups 👍",
+      "Joop 👍",
+      "Asia selvä 👍",
+      "👍",
+      "Kuitti"
+    ),
+    'ok_remove' => array(
+      "Selvä pyy",
+      "Valmis",
+      "Juups",
+      "Joop...",
+      "Poista",
+      "Ok juups",
+      "Juups ok",
+      "Juu kiitoheis",
+      "Juujuu",
+      "Ok juu valmis",
+      "Juu valmis"
+    ),
+    'weekdays' => array(
+      'maanantai',
+      'tiistai',
+      'keskiviikko',
+      'torstai',
+      'perjantai',
+      'lauantai',
+      'sunnuntai'
+    ),
+    'at_weekday' => ''
+  );
+
+  $msg = $msgs[$lang];
+
+?>
