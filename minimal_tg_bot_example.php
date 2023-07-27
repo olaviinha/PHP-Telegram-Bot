df1559b8a63eb4099ab23d9ad919f5dc8416635b
O. Inha <50331907+olaviinha@users.noreply.github.com>
2023-07-27 16:57:43 +0300

    Update and rename even_simpler.php to minimal_tg_bot_example.php

--- minimal_tg_bot_example.php
+++ minimal_tg_bot_example.php
@@ -1,8 +1,9 @@
 <?php
+
   $token = ''; // Telegram bot token
+
   $path = "https://api.telegram.org/bot".$token;
   $update = json_decode(file_get_contents("php://input"), TRUE);
-
   if(array_key_exists('message', $update)){
     $chat_id = $update["message"]["chat"]["id"];
     if(array_key_exists('text', $update["message"])) $message = strip_tags($update["message"]["text"]);
@@ -13,4 +14,5 @@
     $answer = "I just did a thing.";
     file_get_contents($path."/sendmessage?chat_id=".$chat_id."&text=".$answer."&parse_mode=html");
   }
+    
 ?>
